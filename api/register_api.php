71513e40f41068827dcfccda5c5136e2d8162d56
JEJ07 <jjohnemman@gmail.com>
2025-02-16 20:37:11 +0800

    yawa de gumagana

---
+++ api/register_api.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+header("Content-type: application/json");
+include_once __DIR__ . '/../config/database.php';
+include_once __DIR__ . '/../classes/user.php';
+
+
+$database = new Database();
+
+$db = $database->getConnection();
+$user = new User($db);
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+//sanitize
+
+$user->first_name = htmlspecialchars(strip_tags(trim($data['first_name'])));
+$user->last_name = htmlspecialchars(strip_tags(trim($data['last_name'])));
+$user->email = filter_var(trim($data['email']), FILTER_SANITIZE_EMAIL);
+$user->mobile = preg_replace('/\D/', '', trim($data['mobile']));
+$user->role = "user";
+$user->status = "Unverified";
+
+$user->password = htmlspecialchars(strip_tags(trim($data['password'])));
+
+if ($user->password !== $data['confirm_password']) {
+    http_response_code(400);
+    echo json_encode(["message" => "Passwords do not match.", "success" => false]);
+    exit();
+}
+$user->password = password_hash($user->password, PASSWORD_BCRYPT);
+
+if ($user->emailExists()) {
+    http_response_code(409);
+    echo json_encode(["message" => "Email already exists.", "success" => false]);
+    exit();
+}
+//create user
+if ($user->create()) {
+    http_response_code(201);
+    echo json_encode((["message" => "User registered successfully. Pls verify your account.", "success" => true]));
+} else {
+    http_response_code(500);
+    echo json_encode(["message" => "Error registering user.", "success" => false]);
+}
+
+
+?>
