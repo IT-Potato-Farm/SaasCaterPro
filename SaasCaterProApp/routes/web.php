9a91eb0985c4669e6fad1fd0d9f53be5692c0339
Marjouk Ellih <145855414+U-End@users.noreply.github.com>
2025-02-21 22:49:47 +0800

    shitt

--- SaasCaterProApp/routes/web.php
+++ SaasCaterProApp/routes/web.php
@@ -2,12 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-//GET routes examples
 Route::get('/', function () {
     return view('home');
-});
-
-//Parameters using Routes
-Route::get('/portfolio/{firstname}/{lastname}', function ($firstname, $lastname) {
-    return $firstname . " " . $lastname;
 });
