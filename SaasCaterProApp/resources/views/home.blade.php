6c8e98ee48b29564547df9c5429fe870d3c30e0d
Marjouk Ellih <145855414+U-End@users.noreply.github.com>
2025-02-22 13:59:16 +0800

    GET and POST

--- SaasCaterProApp/resources/views/home.blade.php
+++ SaasCaterProApp/resources/views/home.blade.php
@@ -7,5 +7,18 @@
 </head>
 <body>
     <h1>Home</h1>
+    <a href="{{ route("testpage") }}">Go to test page!</a>
+    
+    <form action="{{ route("formsubmitted") }}" method="post">
+        @csrf
+        <label for="fullname">Full name: </label>
+        <input type="text" id="fullname" name="fullname" placeholder="Type your full name!" required>
+        <br><br>
+        <label for="email">E-mail: </label>
+        <input type="text" id="email" name="email" placeholder="Type your e-mail!" required>
+        <br><br>
+        <button type="submit">Submit</button>
+    
+    </form>
 </body>
 </html>
--- SaasCaterProApp/routes/web.php
+++ SaasCaterProApp/routes/web.php
@@ -1,11 +1,50 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
+//GET route example
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/test/{firstname}/{lastname}', function ($firstname, $lastname) {
+
+//Parameters using routes
+Route::get('/parameter/{firstname}/{lastname}', function ($firstname, $lastname) {
     return $firstname . " " . $lastname;
 });
+
+//Named routes using name()
+Route::get('/test', function () {
+    return "This is a Test!";
+})->name("testpage");
+
+//Grouped routes using prefix()
+Route::get('/portfolio', function () {
+    return view('portfolio');
+});
+
+Route::prefix("portfolio")->group(function() {
+    Route::get('/company', function () {
+        return view('company');
+    });
+
+    Route::get('/organization', function () {
+        return view('organization');
+    });
+});
+
+//POST route example
+Route::post("/formsubmitted", function (Request $request) {
+
+    //Validation
+    $request->validate([
+        'fullname' => 'required|min:3|max:30',
+        'email' => 'required|min:3|max:30|email',
+    ]);
+
+    $fullname = $request->input("fullname"); //In PHP, $fullname = $_POST["fullname"];
+    $email = $request->input("email"); //In PHP, $email = $_POST["email"];
+    
+    return "Your full name is {$request->input('fullname')}, and your email is $email!";
+})->name("formsubmitted");
