1e7fc964841b2c42476106c44d8a64c93fef1255
JEJ07 <jjohnemman@gmail.com>
2025-03-07 22:38:38 +0800

    migration of cart and cart items table

--- cater-manage/database/migrations/2025_03_07_143102_create_carts_table.php
+++ cater-manage/database/migrations/2025_03_07_143102_create_carts_table.php
@@ -11,11 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cart_items', function (Blueprint $table) {
+        Schema::create('carts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
-            $table->foreignId('menu_item_id')->constrained('menu_items')->cascadeOnDelete();
-            $table->integer('quantity')->default(1);
+            $table->foreignId('user_id')->nullable()->constrained('users')->nullOnDelete();
             $table->timestamps();
         });
     }
@@ -25,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cart_items');
+        Schema::dropIfExists('carts');
     }
 };
---
+++ cater-manage/database/migrations/2025_03_07_143303_create_cart_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('cart_id')->constrained('carts')->onDelete('cascade');
+            $table->foreignId('menu_item_id')->nullable()->constrained('menu_items')->nullOnDelete();
+            $table->foreignId('package_id')->nullable()->constrained('packages')->nullOnDelete();
+            $table->unsignedInteger('quantity')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cart_items');
+    }
+};
