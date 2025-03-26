b01b4a0239e8017786b8f97566b9ede4629d2269
Marjouk Ellih <145855414+U-End@users.noreply.github.com>
2025-03-26 14:54:07 +0800

    dynamic stars

--- cater-manage/resources/views/components/customer/ratingsection.blade.php
+++ cater-manage/resources/views/components/customer/ratingsection.blade.php
@@ -7,22 +7,23 @@
         @foreach($reviews as $review)
         <div class="block max-w-sm p-6 h-auto w-65 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
             <div class="flex justify-center mb-2">
-                <img src="{{ asset('images\star.svg') }}" class="h-8" alt="Star Icon">
-            </div> 
-            
+                <!-- Loop to display stars based on the rating -->
+                @for($i = 1; $i <= $review->rating; $i++)
+                    <img src="{{ asset('images/star.svg') }}" class="h-6" alt="Star Icon">
+                @endfor
+            </div>
+
             <p class="text-center text-gray-600">Rating: {{ $review->rating }} </p>
 
             @if($review->image)
                 <div class="flex justify-center mb-2">
                     <img src="{{ asset('reviews/' . $review->image) }}" class="h-32 w-auto rounded-lg shadow-md" alt="Review Image">
                 </div>
-                
             @endif
 
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
                 {{ $review->review }}
             </h5>
-           
         </div>
         @endforeach
     @endif
