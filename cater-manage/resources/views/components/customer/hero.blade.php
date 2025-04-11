7d9ae3222ad692768a08f03f4423215b79c22e42
Jimmuel <none>
2025-04-12 06:16:11 +0800

    responsive herosect

--- cater-manage/resources/views/components/customer/hero.blade.php
+++ cater-manage/resources/views/components/customer/hero.blade.php
@@ -1,13 +1,13 @@
-<section class="relative bg-cover bg-center bg-no-repeat" style="background-image: url('{{ asset('images/sectionhero.jpg') }}'); min-height: 100vh;">
+<section class="relative bg-cover bg-center bg-no-repeat max-h-xs md:max-h-screen" style="background-image: url('{{ asset('images/sectionhero.jpg') }}'); ">
     <div class="absolute bg-black bg-opacity-50 inset-0"></div> 
-    <div class="relative z-10 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-        <h1 class="text-8xl font-normal tracking-tight text-white md:text-[16rem] lg:text-[14rem]">
+    <div class="relative z-10 p-10 mx-auto max-w-screen-xl text-center">
+        <h1 class="text-8xl font-normal tracking-tight text-white md:text-9xl lg:text-[14rem]">
             SAAS
         </h1>
         <p class="mb-4 text-lg font-medium tracking-tight leading-snug text-white md:text-2xl lg:text-3xl">
             CATERING AND FOOD SERVICES
         </p>
-        <p class="mb-4 text-lg font-medium tracking-tight leading-snug text-white md:text-2xl lg:text-3xl">
+        <p class="mb-4 md:mb-32 text-lg font-medium tracking-tight leading-snug text-white md:text-2xl lg:text-3xl">
             Offers an exquisite goodness taste of Halal Cuisine
         </p>
     </div>
