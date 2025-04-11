fe3d7ef14677bb2111ded65f7ec3e667d09b8249
Jimmuel <none>
2025-04-12 06:43:56 +0800

    responsive about us

--- cater-manage/resources/views/components/customer/about-us.blade.php
+++ cater-manage/resources/views/components/customer/about-us.blade.php
@@ -1,7 +1,7 @@
-<section id="aboutus" class="flex flex-col items-center justify-center text-center bg-white min-h-[50vh]">
-    <div class="max-w-xl">
-        <h2 class="text-4xl font-extrabold text-gray-900 mb-6">About Us</h2>
-        <p class="text-lg text-gray-700 leading-relaxed ">
+<section id="aboutus" class="flex flex-col items-center justify-center text-justify bg-white mx-10 md:mx-32 lg:mx-48 min-h-[30vh]">
+    <div class="max-w-3/4">
+        <h2 class="text-2xl md:text-4xl font-extrabold text-gray-900 mb-6 text-center">ABOUT US</h2>
+        <p class="text-base sm:text-lg text-gray-700 leading-relaxed">
             Welcome to <span class="font-semibold text-blue-600">SaasCaterPro</span>, your premier choice for exquisite food catering services. 
             With a passion for flavors and a commitment to excellence, we specialize in crafting delicious, high-quality meals 
             tailored to your events. Whether it's a wedding, corporate gathering, or private party, our team ensures an 
@@ -9,3 +9,17 @@
         </p>
     </div>
 </section>
+
+{{-- <section class="flex pt-4 mt-30 flex-col items-center justify-center text-justify bg-white mx-10 md:mx-32 lg:mx-48">
+    <div class="max-w-3/4">
+        <h2 class="text-2xl md:text-4xl font-extrabold text-gray-900 mb-6 text-center">WHY CHOOSE US</h2>
+        <h3 class="text-lg sm:text-2xl md:text-4xl font-normal text-gray-900 mb-6">
+            The all-in-one solution for effortless order tracking and customer engagement services in Zamboanga City.
+        </h3>
+        <p class="text-base sm:text-lg text-gray-700 leading-relaxed">
+            We are more than just your typical catering company at Saas. We are committed to turning your visions into reality.
+            Our goal is to help create an incomparable experience by going above and beyond through our food offerings,
+            service, and styling that is tailored to your taste and budget without skimping on quality.
+        </p>
+    </div>
+</section> --}}
