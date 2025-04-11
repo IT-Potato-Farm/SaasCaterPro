9829bef5e1901cf38020bfaf528a421ed3c7abba
Jimmuel <none>
2025-04-12 07:03:00 +0800

    responsive footer

--- cater-manage/resources/views/components/customer/footer.blade.php
+++ cater-manage/resources/views/components/customer/footer.blade.php
@@ -1,13 +1,13 @@
-<footer class="bg-white rounded-lg shadow-sm dark:bg-gray-900 mt-8 pt-16">
+<footer class=" rounded-lg shadow-sm bg-gray-900 mt-8 md:pt-6">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
-            <div class="holder flex items-center">
-                <img src="{{ asset('images/saaslogo.png') }}" class="h-12">
-                <span class="text-2xl font-semibold whitespace-nowrap dark:text-white">
+            <div class="holder flex items-center  text-center pl-6 pb-6 sm:pl-0">
+                <img src="{{ asset('images/saaslogo.png') }}" class=" h-10 sm:h-12 sm:w-12 md:h-14 md:w-14 lg:h-16 lg:w-16 xl:h-16 xl:w-16 2xl:h-20 2xl:w-20">
+                <span class="text-base md:text-2xl font-semibold whitespace-nowrap text-white">
                     Saas - Catering and Food Services
                 </span>
             </div>
-            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <ul class="flex md:flex-wrap items-center mb-6 pb-10 pl-16 sm:p-0 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400 text-center">
                 <li><a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a></li>
                 <li><a href="#" class="hover:underline me-4 md:me-6">Licensing</a></li>
                 <li><a href="#" class="hover:underline">Contact</a></li>
