00df9bde9ad8cee5e4b3f438db20e256e783f8f4
JEJ07 <jjohnemman@gmail.com>
2025-04-27 17:13:56 +0800

    ver2

--- resources/views/userdashboard.blade.php
+++ resources/views/userdashboard.blade.php
@@ -2,6 +2,8 @@
 <html lang="en">
 
 <head>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <link rel="icon" href="{{ asset('images/saaslogo.png') }}" type="image/png">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
