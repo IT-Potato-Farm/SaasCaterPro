1d21552cc63b5d91577aa1edec6395484c73aa8b
JEJ07 <jjohnemman@gmail.com>
2025-04-29 08:48:46 +0800

    paginatee

--- resources/views/components/items/item-list.blade.php
+++ resources/views/components/items/item-list.blade.php
@@ -217,6 +217,9 @@
                     @endforeach
                 </tbody>
             </table>
+            <div class="mt-4">
+                {{ $items->links() }}
+            </div>
         </div>
     @endif
 </div>
